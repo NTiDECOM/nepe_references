818e615dfe69a504333c95abad6ff2077acdc169
Victor Ximenis <victorximenis@gmail.com>
2015-12-16 01:07:10 -0300

    Search empty results alert

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,8 +2,10 @@ class SearchController < ApplicationController
   def search
     if params[:q].nil?
       @references = []
+      @searched = false
     else
       @references = Reference.search(params[:q])
+      @searched = true
     end
   end
 end
