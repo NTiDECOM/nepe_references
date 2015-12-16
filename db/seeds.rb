f359d0cf43eedc9638438b1a81e1cff5982a52d7
Victor Ximenis <victorximenis@gmail.com>
2015-12-16 01:21:14 -0300

    User Seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+User.create(
+  name: 'Victor Ximenis',
+  email: 'victorximenis@gmail.com',
+  # password: BCrypt::Password.create('12345678'),
+  password: '12345678'
+)
